2f8fd4a4d18ab60a3a26fd516f1aaa61f1a2f84b
prajwal <prajwal@LAPTOP-APS0J7LH>
2019-11-19 18:12:46 +0530

    second

--- demo/src/demo/decimal.java
+++ demo/src/demo/decimal.java
@@ -4,16 +4,16 @@ public class decimal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello\n world");
-		System.out.printf("hello%n world");
-		System.out.println("hello\r world");
-		
-		//binary code
-		byte n = 0b110010;
-		Integer k = new Integer(n);
-		System.out.println(k.toBinaryString(k));
-		
-		  
+//		System.out.println("hello\n world");
+//		System.out.printf("hello%n world");
+//		System.out.println("hello\r world");
+//		
+//		//binary code
+//		byte n = 0b110010;
+//		Integer k = new Integer(n);
+//		System.out.println(k.toBinaryString(k));
+//		
+		  System.out.println("Hello");
 	   
 	   }
 	}
